ed4d5370fb2eceb02c794fc0afe3e80a810238c5
Sandro <layornos@gmail.com>
2020-10-29 01:52:41 +0100

    add base projects

---
+++ mpcm.paradigm.variables/src/variables/impl/PCMRandomVariableImpl.java
@@ -0,0 +1,95 @@
+/**
+ */
+package variables.impl;
+
+import de.uka.ipd.sdq.stoex.impl.RandomVariableImpl;
+
+import java.lang.reflect.InvocationTargetException;
+
+import java.util.Map;
+
+import org.eclipse.emf.common.util.BasicDiagnostic;
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.DiagnosticChain;
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.eclipse.emf.ecore.plugin.EcorePlugin;
+
+import org.eclipse.emf.ecore.util.EObjectValidator;
+
+import variables.PCMRandomVariable;
+import variables.VariablesPackage;
+
+import variables.util.VariablesValidator;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>PCM Random Variable</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class PCMRandomVariableImpl extends RandomVariableImpl implements PCMRandomVariable {
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	protected PCMRandomVariableImpl() {
+		super();
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected EClass eStaticClass() {
+		return VariablesPackage.Literals.PCM_RANDOM_VARIABLE;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	public boolean SpecificationMustNotBeNULL(DiagnosticChain diagnostics, Map<Object, Object> context) {
+		// TODO: implement this method
+		// -> specify the condition that violates the invariant
+		// -> verify the details of the diagnostic, including severity and message
+		// Ensure that you remove @generated or mark it @generated NOT
+		if (false) {
+			if (diagnostics != null) {
+				diagnostics.add
+					(new BasicDiagnostic
+						(Diagnostic.ERROR,
+						 VariablesValidator.DIAGNOSTIC_SOURCE,
+						 VariablesValidator.PCM_RANDOM_VARIABLE__SPECIFICATION_MUST_NOT_BE_NULL,
+						 EcorePlugin.INSTANCE.getString("_UI_GenericInvariant_diagnostic", new Object[] { "SpecificationMustNotBeNULL", EObjectValidator.getObjectLabel(this, context) }),
+						 new Object [] { this }));
+			}
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public Object eInvoke(int operationID, EList<?> arguments) throws InvocationTargetException {
+		switch (operationID) {
+			case VariablesPackage.PCM_RANDOM_VARIABLE___SPECIFICATION_MUST_NOT_BE_NULL__DIAGNOSTICCHAIN_MAP:
+				return SpecificationMustNotBeNULL((DiagnosticChain)arguments.get(0), (Map<Object, Object>)arguments.get(1));
+		}
+		return super.eInvoke(operationID, arguments);
+	}
+
+} //PCMRandomVariableImpl
